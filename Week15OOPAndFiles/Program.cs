8ef4cf3499736eb27399cacde8c6e46d6b8bb164
RomanEnger <Roman.engertzisikov@gmail.com>
2024-04-27 16:57:37 +0300

    add week15 code

---
+++ Week15OOPAndFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿//string[] data = GetDataFromMyFile();
+ReadDataFromArray(GetDataFromMyFile());
+
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\data\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach(string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
